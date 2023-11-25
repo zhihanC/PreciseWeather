0bddfd7e1e870e745e42981726c6b77e57e345b7
zhihanC <67808576+zhihanC@users.noreply.github.com>
2023-11-25 13:28:46 +0100

    Created and formatted CityCards for CitiesScreen

--- app/src/main/java/hu/ait/preciseweather/screen/CitiesScreen.kt
+++ app/src/main/java/hu/ait/preciseweather/screen/CitiesScreen.kt
@@ -1,9 +1,24 @@
 package hu.ait.preciseweather.screen
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -12,10 +27,17 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CitiesScreen() {
+fun CitiesScreen(
+    modifier: Modifier = Modifier
+) {
     Column() {
         TopAppBar(
             title = { Text(text = "Precise Weather") },
@@ -39,5 +61,66 @@ fun CitiesScreen() {
                 }
             }
         )
+
+        Column() {
+            CityCard("Budapest")
+        }
+    }
+}
+
+@Composable
+fun CityCard(
+    cityName: String
+) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant),
+        shape = RoundedCornerShape(20.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 10.dp),
+        modifier = Modifier.padding(5.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(5.dp)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio =
+                        Spring.DampingRatioHighBouncy,
+                        stiffness = Spring.StiffnessMedium
+                    )
+                )
+        ) {
+            Row(
+                modifier = Modifier
+                    .padding(20.dp)
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = cityName,
+                    modifier = Modifier.fillMaxWidth(0.5f)
+                )
+                Spacer(modifier = Modifier.width(80.dp))
+                Icon(
+                    imageVector = Icons.Filled.Info,
+                    contentDescription = "Indo",
+//                    modifier = Modifier.clickable {
+//                        onRemoveItem()
+//                    },
+                    tint = Color.Blue
+                )
+                Icon(
+                    imageVector = Icons.Filled.Delete,
+                    contentDescription = "Delete",
+//                    modifier = Modifier.
+//                        .clickable {
+//                        onRemoveItem()
+//                    },
+                    tint = Color.Red
+                )
+            }
+        }
     }
 }
